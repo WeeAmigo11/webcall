50309e77c3a5fe5da7d524ba850efe0ea3f030ed
WeeAmigo <77457724+WeeAmigo11@users.noreply.github.com>
2025-09-11 20:28:20 +0300

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	NODE_ENV: process.env.NODE_ENV,
-	PORT: process.env.PORT ?? 824,
+	PORT: process.env.PORT ?? 8888,
 	CORS_ORIGIN: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(",") : ["http://localhost:824"],
 };
